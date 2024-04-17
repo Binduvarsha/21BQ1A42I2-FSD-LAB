42b4abfb81106be8d6964f5f84dcc2e20f367b43
Binduvarsha <108585890+Binduvarsha@users.noreply.github.com>
2024-04-17 21:54:36 +0530

    Updated

--- 19.CRUD/schema/server1.js
+++ 19.CRUD/schema/server1.js
@@ -1,3 +1,14 @@
+// In db-crud folder not in src, Create with name server1.js
+//npm install mongoose
+//first terminal: node server1.js
+//for CRUD Operations: in another terminal
+//Invoke-RestMethod -Method Post -Uri "http://localhost:5000/api/users" -ContentType "application/json" -Body '{"name": "John Doe", "email": "john@example.com", "age": 30}'
+//Invoke-RestMethod -Method Post -Uri "http://localhost:5000/api/users" -ContentType "application/json" -Body '{"name": "Alex", "email": "john@yahoo.com", "age": 35}' 
+//Invoke-RestMethod -Method Get -Uri "http://localhost:5000/api/users"
+//Invoke-RestMethod -Method Get -Uri "http://localhost:5000/api/users/65cb829decab01d45546620b" (65cb829decab01d45546620b--userID in DB)
+//Invoke-RestMethod -Method Put -Uri "http://localhost:5000/api/users/65cb829decab01d45546620b" -ContentType "application/json" -Body '{"name": "Siri", "email": "siri@in.com", "age": 25}'  
+//Invoke-RestMethod -Method Delete -Uri "http://localhost:5000/api/users/65cb821aecab01d455466209"
+
 // const express=require('express');
 // const mongoose=require('mongoose');
 // const bodyParser=require('body-parser');
